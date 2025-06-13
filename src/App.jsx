d6a29294e30ed37d1fc9791c9c8b9d0058d9f650
= <=>
2025-06-13 20:38:44 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PainelUsuario from './components/PainelUsuario';
 import PainelAtendente from './components/PainelAtendente';
-import Home from './pages/Home'; // <-- novo
+import Home from './pages/Home'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Agora exibe a tela inicial */}
+        <Route path="/" element={<Home />} /> 
         <Route path="/usuario" element={<PainelUsuario />} />
         <Route path="/atendente" element={<PainelAtendente />} />
       </Routes>
